de476457346f15699b6287d32221e6d12352f3c3
Lam207 <113150246+Lam207@users.noreply.github.com>
2022-10-02 15:52:45 -0400

    Update DrivableTrader.java

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,37 @@
  */
 
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable>{
+    /** Constructor of the DrivableTrader class
+     *
+     * @param inventory the inventory of the trader (from the parent class Trader)
+     * @param wishlist  the wishlist of the trader (from the parent class Trader)
+     * @param price     the amount of money the trader has (from the parent class Trader)
+     */
+    public DrivableTrader(List<Drivable> inventory,
+                          List<Drivable> wishlist,
+                          int price){
+        super(inventory, wishlist, price);
+    }
+    /** Another overridden constructor of the DrivableTrader class
+     *
+     * @param price     the amount of money the trader has (from the parent class Trader)
+     */
+
+    public DrivableTrader(int price){ super(price); }
+
+    /** A method to get the selling price of a Drivable object, which will take in
+     * a Drivable object and return its price. Return 0 if there is no price.
+     *
+     * @param object a Drivable object
+     *
+     * @return the price of the given Drivable object
+     */
+    public int sellingPrice(Drivable object){
+        if (object instanceof Tradable){
+            return ((Tradable) object).getPrice() + object.getMaxSpeed();
+        }
+        return 0;
+    }
+}
