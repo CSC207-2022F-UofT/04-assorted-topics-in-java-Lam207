ca8dd231a0aad3752f731a6e52e223fc214d6a17
Lam207 <113150246+Lam207@users.noreply.github.com>
2022-10-02 15:57:46 -0400

    Update DrivableMap.java

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -26,7 +26,18 @@ class DrivableMap {
      *       and a Drivable object. If the ID string does not appear as a key
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
+     * @param id        the id of a Drivable object
+     *        object    a given Drivable object
+     *                  
+     * @return True if a new Drivable object is added, and False otherwise.
      */
+    public boolean addDrivable(String id, Drivable object){
+        if (!drivable_map.containsKey(id)) {
+            drivable_map.put(id, object);
+            return true;
+        }
+        return false;
+    }
 
 
 
@@ -36,7 +47,19 @@ class DrivableMap {
      *       that has a maxSpeed >= the speed given.
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
+     * @param speed a given speed
+     *              
+     * @return True if there is any Drivable object that has a higher speed than given
+     *         speed, and False otherwise. 
      */
+    public boolean hasFasterThan(int speed){
+        for (String key: drivable_map.keySet()){
+            if (drivable_map.get(key).getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -45,9 +68,18 @@ class DrivableMap {
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
+     *       
+     * @return a list of Tradable objects.
      */
+    public List<Tradable> getTradable(){
+        List<Tradable> tradable = new ArrayList<>();
+        for (String key: drivable_map.keySet()) {
+            if (drivable_map.get(key) instanceof Tradable) {
+                Tradable alias = (Tradable) drivable_map.get(key);
+                tradable.add(alias);
+            }
+        }
+        return tradable;
+    }
 
-
-
-    
 }
