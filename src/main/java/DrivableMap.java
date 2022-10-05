b136cd584f1cb26036546a632f3f49eeba051c68
Lam207 <113150246+Lam207@users.noreply.github.com>
2022-10-04 20:10:11 -0400

    Update DrivableMap.java

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -22,13 +22,14 @@ class DrivableMap {
         drivable_map = new HashMap<>();
     }
 
-    /* TODO: Write a method named addDrivable that takes a String (the ID)
+    /** TODO: Write a method named addDrivable that takes a String (the ID)
      *       and a Drivable object. If the ID string does not appear as a key
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
+     *
      * @param id        the id of a Drivable object
      *        object    a given Drivable object
-     *                  
+     *
      * @return True if a new Drivable object is added, and False otherwise.
      */
     public boolean addDrivable(String id, Drivable object){
@@ -42,15 +43,15 @@ class DrivableMap {
 
 
 
-    /* TODO: Write a method named hasFasterThan that takes an int (a speed)
+    /** TODO: Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
      *       that has a maxSpeed >= the speed given.
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      * @param speed a given speed
-     *              
+     *
      * @return True if there is any Drivable object that has a higher speed than given
-     *         speed, and False otherwise. 
+     *         speed, and False otherwise.
      */
     public boolean hasFasterThan(int speed){
         for (String key: drivable_map.keySet()){
@@ -65,10 +66,10 @@ class DrivableMap {
 
 
 
-    /* TODO: Write a method named getTradable that takes no arguments and
+    /** TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
-     *       
+     *
      * @return a list of Tradable objects.
      */
     public List<Tradable> getTradable(){
@@ -82,4 +83,5 @@ class DrivableMap {
         return tradable;
     }
 
+    
 }
